fece28b821bc20b9623be9251ab3b61b4597c52a
Debanshu060 <120ME0296@nitrkl.ac.in>
2021-08-11 12:54:38 +0530

    removed the while loop

--- apriltag_detector/scripts/node1.py
+++ apriltag_detector/scripts/node1.py
@@ -4,25 +4,23 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge,CvBridgeError
 
+
 class listener():
     def __init__(self):
         rospy.init_node('listener',anonymous = True)
         self.sub = rospy.Subscriber("/overhead_camera/image_raw",Image,self.callback)
-        self.rate = rospy.Rate(1)
+        #self.rate = rospy.Rate()
         self.bridge = CvBridge()
 
     def callback(self,data):
         try :
-            while not rospy.is_shutdown():
-                #print("entering1")
-                self.image = self.bridge.imgmsg_to_cv2(data,"bgr8")
-                cv2.imshow("image",self.image)
-                if cv2.waitKey(20) == ord('q'):
-                    break
-
+            self.image = self.bridge.imgmsg_to_cv2(data,"bgr8")
+            cv2.imshow("image",self.image)
+            cv2.waitKey(34)
             cv2.destroyAllWindows()
         except CvBridgeError as e:
             print(e)
+
 
 
 if __name__ == '__main__':
