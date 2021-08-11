2ebf1fdcfab93ca971b001f79d045675a1864aaf
Debanshu060 <120ME0296@nitrkl.ac.in>
2021-08-11 23:21:30 +0530

    added apriltag detection algorithm

--- apriltag_detector/scripts/node1.py
+++ apriltag_detector/scripts/node1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 import cv2
+import apriltag
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge,CvBridgeError
 
@@ -15,6 +16,22 @@ class listener():
     def callback(self,data):
         try :
             self.image = self.bridge.imgmsg_to_cv2(data,"bgr8")
+            self.gray = cv2.cvtColor(self.image,cv2.COLOR_BGR2GRAY)
+
+            options = apriltag.DetectorOptions('tag36h11')
+            detector = apriltag.Detector(options)
+            results = detector.detect(self.gray)
+
+            for i in results:
+                pta , ptb ,ptc ,ptd = i.corners
+                cn = i.center
+                pta = (int(pta[0]),int(pta[1]))
+                ptc = (int(ptc[0]),int(ptc[1]))
+                cn = (int(cn[0]),int(cn[1]))
+
+                cv2.rectangle(self.image,pta,ptc,(0,255,0),2)
+                cv2.circle(self.image,cn,3,(0,0,255),-1)
+
             cv2.imshow("image",self.image)
             cv2.waitKey(34)
             cv2.destroyAllWindows()
