9bf527eadf75f4537f385d07ef4cf355787a666f
anjan-mridha <120cr0648@nitrkl.ac.in>
2021-09-18 08:19:39 +0530

    esp_websocket added

---
+++ grid_hardware/grid_transmitter/src/esp_websocket.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+import websocket
+import rospy
+
+from std_msgs.msg import String,Int32
+class listen():
+    def __init__(self,host):
+        rospy.init_node("wifi_transmitter",anonymous=True)
+        self.ws= websocket.Websocket()
+        self.ws.connect("ws://"+host)
+        self.sub = rospy.Subscriber("wifi",String,self.callback)
+
+    def callback(self,data):
+        try:
+            msg = str(data.data)
+            print(msg)
+            self.ws.send(msg)
+        except rospy.ROSInterruptException as e:
+            print(e)
+            self.ws.close()
+
+if __name__ == '__main__':
+    obj = listen("192.168.x.x:8888")
+    try:
+        rospy.spin()
+    except Exception as e:
+        print(e)
