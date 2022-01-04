444439fdf20c6689f7bfd7d33581431b521ed1f7
anjan-mridha <120cr0648@nitrkl.ac.in>
2022-01-04 18:24:25 +0530

    normal updates

--- grid_control/grid_phase2_controller/src/controller.py
+++ grid_control/grid_phase2_controller/src/controller.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import String, Int32
+from std_msgs.msg import String
 from geometry_msgs import Twist
 import math
 
@@ -23,7 +23,8 @@ class motion():
         rospy.init_node("bot_motion", anonymous=True)
         self.sub1 = rospy.Subscriber(
             "apriltag_centre1", String, self.callback_pose)
-        # self.sub2= rospy.Subscriber("Co-ordinates from client",String , self.callback_co_ord)
+        # self.sub2= rospy.Subscriber("Co-ordinates from client",
+        # String , self.callback_co_ord)
         self.pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
         self.rate = rospy.Rate(10)
         self.msg = Twist()
@@ -33,8 +34,8 @@ class motion():
         self.center = [pose[0], pose[1]]
         self.ang = pose[2]
 
-    # def callback_co_ord(self,data):
-    #     self.destination = list(data.data.split(" "))
+    def callback_co_ord(self, data):
+        self.destination = list(data.data.split(" "))
 
     def move(self):
         x1 = 304-self.center[0]
@@ -46,7 +47,7 @@ class motion():
             ang += 360
         if ang > 180:
             ang -= 360
-        final_speed = pid(dist, ang)
+        final_speed = list(self.pid(dist, ang))
         vel = 100 + final_speed[0]*5
         if dist < 5:
             self.msg.linear.x = 0
@@ -57,7 +58,8 @@ class motion():
         self.pub.publish(self.msg)
 
     def pid(self, dist, ang):
-        global kp_ang, ki_ang, kd_ang, kp_dist, ki_dist, kd_dist, prev_dist, prev_ang, I_dist, I_ang
+        global kp_ang, ki_ang, kd_ang, kp_dist, ki_dist
+        global kd_dist, prev_dist, prev_ang, I_dist, I_ang
         err_ang = ang
         err_dist = dist
         diff_dist = err_dist - I_dist
