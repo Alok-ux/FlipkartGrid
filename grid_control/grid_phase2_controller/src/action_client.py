5ac822e070acf35c99c0d33d09667c37c16f99d0
anjan-mridha <120cr0648@nitrkl.ac.in>
2022-01-10 01:53:19 +0530

    action_client modified

--- grid_control/grid_phase2_controller/src/action_client.py
+++ grid_control/grid_phase2_controller/src/action_client.py
@@ -26,15 +26,15 @@ class InductStation:
         self.__induct_list = []   # packages available at the induct station
         self.__read_csv(path)     # read package data from csv file
 
-        self.__drop_pose = {'Mumbai': [(4, 12), (4, 13)],
-                            'Delhi': (8, 12),
-                            'Kolkata': (12, 12),
-                            'Chennai': (4, 8),
-                            'Bengaluru': (8, 8),
-                            'Hyderabad': (12, 8),
-                            'Pune': (4, 4),
-                            'Ahmedabad': (8, 4),
-                            'Jaipur': (12, 4)}      # position of drop location
+        self.__drop_pose = {'Mumbai': (3, 9),
+                            'Delhi': (7, 9),
+                            'Kolkata': (11, 9),
+                            'Chennai': (3, 5),
+                            'Bengaluru': (7, 5),
+                            'Hyderabad': (11, 5),
+                            'Pune': (3, 1),
+                            'Ahmedabad': (7, 1),
+                            'Jaipur': (11, 1)}      # position of drop location
 
     def __read_csv(self, path: str) -> None:
         '''
@@ -48,7 +48,7 @@ class InductStation:
                 if i == 0:
                     continue
                 if int(row[1]) == self.id:
-                    self.induct_list.append(row[-1])
+                    self.__induct_list.append(row[-1])
 
     def __len__(self) -> int:
         '''
@@ -56,7 +56,7 @@ class InductStation:
         params: None
         return: (int): length of the induct list
         '''
-        return len(self.induct_list)
+        return len(self.__induct_list)
 
     def __iter__(self) -> iter:
         '''
@@ -64,7 +64,7 @@ class InductStation:
         params: None
         return: (iter): iterator of induct list
         '''
-        return iter(self.induct_list)
+        return iter(self.__induct_list)
 
     def __getitem__(self, key: str) -> list:
         '''
@@ -80,20 +80,21 @@ class InductStation:
         params: None
         return: (int): poped entry from the beginning
         '''
-        return self.induct_list.pop(0)
+        return self.__induct_list.pop(0)
 
 
 class Robot:
     def __init__(self, id):
         self.client = actionlib.SimpleActionClient(
             'grid_robot_{}'.format(id), botAction)
-        self.client.wait_for_server()
+        # self.client.wait_for_server()
+        self.goal = list()
 
-    def preempt_goal(self):
-        self.client.cancel_goal()
+    def send_goal(self):
+        x, y = self.goal.pop(0)
+        x_next, y_next = self.goal[0]
 
-    def send_goal(self, goal):
-        bot_goal = botGoal(x=goal[0], y=goal[1])
+        bot_goal = botGoal(x=x, y=y, phi=0)
         self.client.send_goal(bot_goal)
 
 
@@ -110,48 +111,31 @@ class Automata:
                 print(exc)
 
     def execute(self, num_pkg=-1):
+        self.induct_station[0].pop()
+        self.induct_station[1].pop()
         self.param['agents'] = list()
         for i in range(len(self.bots)):
             self.param['agents'].append({'start': self.induct_station[i].pose,
-                                        'goal': self.induct_station[i][self.induct_station[i].pop()],
+                                         'goal': self.induct_station[i][self.induct_station[i].pop()],
                                          'name': 'agent'+str(i)
                                          })
         print(self.param)
         solution, _ = solve(self.param)
         print(solution)
-        # for i, bot in enumerate(self.bots):
-        #     bot.send_goal(solution['agent'+str(i)])
 
-    # def new_goal(self, station_id):
-    #     return self.induct_station[station_id].pop()
-    #
-    # def done_cb(self, status, result):
-    #     for i, bot in enumerate(self.bots):
-    #         if result.result.id == bot.id:
-    #             # Goto induct station
-    #             self.param['agents'][i]['start'] = bot.curr_pose
-    #             self.param['agents'][i]['goal'] = self.induct_station[i//4].pose
-    #             self.param['agents'][i]['name'] = 'agent'+str(i)
-    #
-    #             # Already in induct station & goto new destination
-    #             for i in self.induct_station:
-    #                 if i.pose == bot.curr_pose:
-    #                     bot.prev_goal = self.param['agents'][i]['goal'] = self.induct_station[i].pop(
-    #                     )
-    #                     time.sleep(2)
-    #                     break
-    #             continue
-    #
-    #         bot.preempt_goal()
-    #         self.param['agents'][i]['start'] = bot.curr_pose
-    #         self.param['agents'][i]['goal'] = bot.prev_goal
-    #         self.param['agents'][i]['name'] = 'agent'+str(i)
-    #
-    #     # Get path from CBS algorithm
-    #     solution, _ = solve(self.param)
-    #
-    #     for i, bot in enumerate(self.bots):
-    #         bot.send_goal(solution['agent'+str(i)])
+        # TODO: Send goal to server
+        for i, bot in enumerate(self.bots):
+            bot.goal = [(d['x'], d['y']) for d in solution['agent'+str(i)]]
+
+        while not rospy.is_shutdown():
+            try:
+                for bot in self.bots:
+                    bot.send_goal()
+                for bot in self.bots:
+                    bot.wait_for_result()
+                pass
+            except IndexError:
+                break
 
 
 if __name__ == '__main__':
@@ -159,7 +143,7 @@ if __name__ == '__main__':
         rospy.init_node('automata')
         rospack = rospkg.RosPack()
         path = rospack.get_path('grid_phase2_controller') + "/data/"
-        induct_station_pose = [(1, 2), (3, 4)]
+        induct_station_pose = [(0, 4), (0, 9)]
         automata = Automata(2, induct_station_pose,
                             path+"Sample Data - Sheet1.csv", path+"input.yaml")
         automata.execute()
