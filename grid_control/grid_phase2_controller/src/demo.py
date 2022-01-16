39e7779832f5f8e146a2d95fad799c4025aafcae
anjan-mridha <120cr0648@nitrkl.ac.in>
2022-01-16 14:33:36 +0530

    hyper_param modified

--- grid_control/grid_phase2_controller/src/demo.py
+++ grid_control/grid_phase2_controller/src/demo.py
@@ -17,10 +17,12 @@ client2.wait_for_server()
 
 # pose_list = [(221, 360), (219, 318), (184, 321), (180, 285),
 #              (141, 284), (100, 283), (101, 286)]
-pose_list1 = [(5, 2), (5, 4), (3, 4), (3, 6), (5, 6)]
-pose_list2 = [(6, 2), (6, 4), (5, 4), (3, 4), (3, 6)]
-pose_list1.append((5, 9))
-for i in range(1, len(pose_list1)):
+pose_list1 = [(0, 4), (1, 4), (1, 5), (2, 5), (3, 5),
+              (4, 5), (5, 5), (5, 4), (6, 4), (7, 4)]
+pose_list2 = [(0, 9), (1, 9), (1, 8), (2, 8),
+              (3, 8), (4, 8), (5, 8), (5, 7), (5, 6), (4, 6)]
+pose_list1.append((7, 5))
+for i in range(1, 10):
     if i == len(pose_list1)-1:
         pose_list1.append((5, 9))
         x_i, y_i = pose_list1[i][0], pose_list1[i][1]
@@ -35,12 +37,12 @@ for i in range(1, len(pose_list1)):
         phi = math.degrees(math.atan2(y_i - y_j, x_j - x_i))
         goal1 = botGoal(x=x_i, y=y_i, phi=phi)
 
-    # x_i, y_i = pose_list2[i][0], pose_list2[i][1]
-    # x_j, y_j = pose_list2[i+1][0], pose_list2[i+1][1]
-    # phi = math.degrees(math.atan2(y_i - y_j, x_j - x_i))
-    # goal2 = botGoal(x=x_i, y=y_i, phi=phi)
+    x_i, y_i = pose_list2[i][0], pose_list2[i][1]
+    x_j, y_j = pose_list2[i+1][0], pose_list2[i+1][1]
+    phi = math.degrees(math.atan2(y_i - y_j, x_j - x_i))
+    goal2 = botGoal(x=x_i, y=y_i, phi=phi)
 
     client1.send_goal(goal1)
-    # client2.send_goal(goal2)
+    client2.send_goal(goal2)
     print(client1.wait_for_result())
-    # print(client2.wait_for_result())
+    print(client2.wait_for_result())
--- grid_hardware/camera_driver/src/pose_publisher.py
+++ grid_hardware/camera_driver/src/pose_publisher.py
@@ -33,8 +33,9 @@ class PoseCamDriver:
             # cap.set(3, 640)
             # cap.set(4, 480)
             # frame = frame[1:479, 41:593]
-            # frame = frame[103:452, 113:564]
-            frame = frame[5:480, 40:593]
+            #frame = frame[103:452, 113:564]
+            #frame = frame[5:480, 40:593]
+            frame = frame[23:473, 43:575]
 
             gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
             results = self.detector.detect(gray)
